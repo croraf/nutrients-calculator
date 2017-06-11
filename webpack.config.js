5fb852f5f0f0581580f3a0761a427acf19da0626
Korisnik <vrafaeli@msn.com>
2017-06-12 00:18:15 +0200

    webpack config minor change

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        publicPath: '/dist/'
+        publicPath: 'dist/'
     },
     resolve: {
         modules: [
