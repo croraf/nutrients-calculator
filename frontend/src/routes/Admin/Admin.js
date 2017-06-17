eee3240275d0a2ac543731d452ad9cc43cb61e8a
Korisnik <vrafaeli@msn.com>
2017-06-17 23:09:01 +0200

    Making create ingredient as dialog

--- frontend/src/routes/Admin/Admin.js
+++ frontend/src/routes/Admin/Admin.js
@@ -2,12 +2,57 @@ import React from 'react';
 
 import {NutrientForm} from './NutrientForm/NutrientForm';
 
+import RaisedButton from 'material-ui/RaisedButton';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+
 class Admin extends React.Component {
+
+    state = {
+        open: false,
+    }
+
+    handleOpen = () => {
+        this.setState({open: true});
+    }
+
+    handleClose = () => {
+        this.setState({open: false});
+    }
+
     render () {
+
+        const actions = [
+            <FlatButton
+            label="Save ingredient"
+            primary={true}
+            keyboardFocused={true}
+            onTouchTap={this.handleClose}
+            />,
+            <FlatButton
+            label="Cancel"
+            primary={true}
+            onTouchTap={this.handleClose}
+            />
+        ];
+
         return (
             <div style={{padding: '10px'}}>
-                <NutrientForm onSubmit={(values) => {console.log('submitting: ' + JSON.stringify(values));}} />
+
+                <RaisedButton label="Add ingredient" onTouchTap={this.handleOpen} />
+                <Dialog
+                    title="Define ingredient"
+                    actions={actions}
+                    modal={false}
+                    open={this.state.open}
+                    onRequestClose={this.handleClose}
+                    autoScrollBodyContent={true}
+                    >
+                    
+                    <NutrientForm onSubmit={(values) => {console.log('submitting: ' + JSON.stringify(values));}} />
+                </Dialog>
             </div>
+            
         );
     }
 }
