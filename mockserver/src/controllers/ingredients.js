bc3b9f7c5d50649d17f72fa8934d2d7f5699c447
Korisnik <vrafaeli@msn.com>
2017-06-22 19:38:35 +0200

    Adding swagger

--- mockserver/src/controllers/ingredients.js
+++ mockserver/src/controllers/ingredients.js
@@ -1,12 +1,22 @@
-
-const getIngredients = async (ctx, next) => {
-    let responseBody = ['apple', 'chocolate, white', 'chocolate, dark 70%', 'chocolate milk, 1%', 'chocolate milk, 3%',
+const ingredients = [
+    'apple', 'chocolate, white', 'chocolate, dark 70%', 'chocolate milk, 1%', 'chocolate milk, 3%',
         'banana', 'orange', 'oat leafs', 'rice, white', 'rice, brown', 'bread, white', 'pineapple', 'pear', 'cherry', 'milk, 3%',
         'milk, 1%', 'tomato', 'potato', 'olive oil',
-        'sunflower oil', 'beans, kidney'];
-    
-    ctx.body = responseBody; //responseBody.map((item) => ctx.params.ingredient + '---' + item);
+        'sunflower oil', 'beans, kidney'
+];
+
+const getIngredients = async (ctx, next) => {
+    console.log('ingredients');
+    ctx.body = ingredients; //responseBody.map((item) => ctx.params.ingredient + '---' + item);
 };
 
-export {getIngredients};
+const addIngredient = async (ctx, next) => {
+
+    console.log(ctx.request.body);
+    ingredients.push(ctx.request.body);
+
+    ctx.body = ctx.request.body;
+}
+
+export default {addIngredient, getIngredients};
 
