d72cbb3de6d7ee2dd8fdda5f2bc57e944c3f4d3c
Korisnik <vrafaeli@msn.com>
2017-06-24 20:13:35 +0200

    Remove token from redux state.
    It is in localstorage, in redux will be name and role

--- frontend/src/modules/login.js
+++ frontend/src/modules/login.js
@@ -9,18 +9,21 @@ const sendLoginRequest = (credentials) => (dispatch) => {
     sendLoginRequestAPI(credentials)
         .then(token => {
             
+            console.log('token:', token);
+
             localStorage.setItem('wholeprotein_token', token);
 
             const payloadEncoded = token.split('.')[1];
             const payload = JSON.parse(new Buffer(payloadEncoded, 'base64').toString('ascii'));
+
             console.log('payload from token:', payload);
 
-            dispatch({type: 'TOKEN_RECEIVED', token: token, name: payload.name});
+            dispatch({type: 'TOKEN_RECEIVED', name: payload.name});
 
             dispatch(push('/calculator'));
         }).catch(reason => {
             console.log('unauthorized', reason);
-            dispatch({type: 'UNAUTHORIZED', token: ''});
+            dispatch({type: 'UNAUTHORIZED'});
         });
     
 };
@@ -29,27 +32,26 @@ const logOut = () => (dispatch)=> {
 
     localStorage.removeItem('wholeprotein_token');
 
-    dispatch ({type: 'LOG_OUT'});
+    dispatch({type: 'LOG_OUT'});
 };
 
-const loginReducer = (state={loginInProgress: false, token: '', name: '', role: '', authorized: false, rejected: false}, action) => {
+const loginReducer = (state={loginInProgress: false, name: '', role: '', authorized: false, rejected: false}, action) => {
 
     switch (action.type) {
         case 'LOGIN_REQUEST_STARTED':
             console.log('LOGIN_REQUEST_STARTED', action.credentials);
             return Object.assign({}, state, {loginInProgress: true});
         case 'TOKEN_RECEIVED':
-            console.log('TOKEN_RECEIVED: ', action.token);
+            console.log('TOKEN_RECEIVED');
             return Object.assign({}, state, {
                 loginInProgress: false,
-                token: action.token,
                 name: action.name,
                 authorized: true,
                 rejected: false}
             );
         case 'LOG_OUT':
             console.log('LOG_OUT');
-            return Object.assign({}, state, {token: '', name: '', role: '', authorized: false});
+            return Object.assign({}, state, {name: '', role: '', authorized: false});
         case 'UNAUTHORIZED':
             console.log('UNAUTHORIZED');
             return Object.assign({}, state, {loginInProgress: false, rejected: true}); 
