33c8bb6272e57ab6cc0d384554f420e824e874fe
Korisnik <vrafaeli@msn.com>
2017-08-20 16:58:25 +0200

    Dont show suggestions on 1 letter input.

--- frontend/src/routes/Calculator/CalculatorForm/MyAutosuggest.js
+++ frontend/src/routes/Calculator/CalculatorForm/MyAutosuggest.js
@@ -27,7 +27,7 @@ class MyAutosuggest extends React.Component {
         const inputValue = value.trim().toLowerCase();
         const inputLength = inputValue.length;
 
-        return inputLength === 0 ? [] : this.props.dataSource.map(food => food.name).filter(ingredient =>
+        return inputLength <= 1 ? [] : this.props.dataSource.map(food => food.name).filter(ingredient =>
             ingredient.toLowerCase().slice(0, inputLength) === inputValue
         );
     }
