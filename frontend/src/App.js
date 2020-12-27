9a6a5cccf5d62be0a28359fcd3fea904266c224e
croraf <vrafaeli@msn.com>
2020-12-27 22:56:40 +0100

    Minor frontend update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { Route, Redirect } from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router';
@@ -14,6 +14,7 @@ import { createMuiTheme } from '@material-ui/core/styles';
 
 import { Header } from './Common/Header/Header';
 import { AdvertisementFooter } from './Common/Advertisement/AdvertisementFooter';
+import { useDispatch, useSelector } from 'react-redux';
 
 const muiTheme = createMuiTheme({
     /* datePicker: {
@@ -21,61 +22,49 @@ const muiTheme = createMuiTheme({
     } */
 });
 
-class App extends React.Component {
+const App = () => {
+    const dispatch = useDispatch();
+    const name = useSelector(state => state.login.name);
 
     // LOAD THINGS FROM LOCAL STORAGE
-    componentDidMount() {
-
+    useEffect(() => {
         const token = localStorage.getItem('wholeprotein_token');
-
         if (token) {
             const payloadEncoded = token.split('.')[1];
-            const payload = JSON.parse(new Buffer(payloadEncoded, 'base64').toString('ascii'));
-
-            this.props.dispatch({ type: 'TOKEN_RECEIVED', name: payload.name });
+            const payload = JSON.parse(Buffer.from(payloadEncoded, 'base64').toString('ascii'));
+            dispatch({ type: 'TOKEN_RECEIVED', name: payload.name });
         }
-    }
+    }, [dispatch]);
 
-    render() {
+    return (
+        <ThemeProvider theme={muiTheme}>
+            <div style={{ minWidth: '400px' }}>
+                <Header />
+                <ConnectedRouter history={history}>
+                    <div>
+                        <Route path='/' exact render={() => <Redirect to='/login' />} />
+                        <Route path="/login" component={
+                            (props) => (name ? <Redirect to='/calculator' /> : <DynamicRouteContainer {...props} />)
+                        } />
+                        <Route path="/calendar" component={
+                            (props) => <DynamicRouteContainer {...props} secure />
+                        } />
+                        <Route path="/calculator" component={
+                            (props) => <DynamicRouteContainer {...props} secure />
+                        } />
+                        <Route path="/statistics" component={
+                            (props) => <DynamicRouteContainer {...props} secure />
+                        } />
+                        <Route path="/profile" component={
+                            (props) => <DynamicRouteContainer {...props} secure />
+                        } />
+                        {/* <Route path="/admin" component={AdminRoute} /> */}
+                    </div>
+                </ConnectedRouter>
+                <AdvertisementFooter />
+            </div>
+        </ThemeProvider>
+    );
+};
 
-        return (
-            <ThemeProvider theme={muiTheme}>
-                <div style={{ minWidth: '400px' }}>
-                    <Header />
-
-                    <ConnectedRouter history={history}>
-                        <div>
-                            <Route path='/' exact render={() => <Redirect to='/login' />} />
-
-                            <Route path="/login" component={
-                                (props) => (this.props.name ? <Redirect to='/calculator' /> : <DynamicRouteContainer {...props} />)
-                            } />
-
-                            <Route path="/calendar" component={
-                                (props) => <DynamicRouteContainer {...props} secure />
-                            } />
-
-                            <Route path="/calculator" component={
-                                (props) => <DynamicRouteContainer {...props} secure />
-                            } />
-
-                            <Route path="/statistics" component={
-                                (props) => <DynamicRouteContainer {...props} secure />
-                            } />
-
-                            <Route path="/profile" component={
-                                (props) => <DynamicRouteContainer {...props} secure />
-                            } />
-
-                            {/* <Route path="/admin" component={AdminRoute} /> */}
-                        </div>
-                    </ConnectedRouter>
-
-                    <AdvertisementFooter />
-                </div>
-            </ThemeProvider>
-        );
-    }
-}
-
-export { App };
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,13 +6,13 @@ import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
 import {store} from './modules/store';
-import {AppContainer} from './AppContainer';
+import App from './App';
 
 
 
 ReactDOM.render(
     <Provider store={store}>
-        <AppContainer />
+        <App />
     </Provider>,
     document.getElementById('root')
 );
